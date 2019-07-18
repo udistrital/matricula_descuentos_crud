0b731ca6a80eaa378c4ea0eab0f1da492c81d179
Fabian Barreto <fdbarretos@correo.udistrital.edu.co>
2019-07-18 16:07:47 -0500

    Se configura app.conf y el main.go con variables de entorno

--- main.go
+++ main.go
@@ -1,18 +1,58 @@
 package main
 
 import (
-	_ "github.com/udistrital/descuento_academica_crud/routers"
+	_ "github.com/udistrital/descuento_academico_crud/routers"
+	apistatus "github.com/udistrital/utils_oas/apiStatusLib"
+	"github.com/udistrital/utils_oas/customerror"
 
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/logs"
 	"github.com/astaxie/beego/orm"
+	"github.com/astaxie/beego/plugins/cors"
 	_ "github.com/lib/pq"
 )
 
+func test() {
+	if beego.BConfig.RunMode == "dev" {
+		orm.Debug = true
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+}
+
+func init() {
+	test()
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"PUT", "PATCH", "GET", "POST", "OPTIONS", "DELETE"},
+		AllowHeaders: []string{"Origin", "x-requested-with",
+			"content-type",
+			"accept",
+			"origin",
+			"authorization",
+			"x-csrftoken"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+}
 func main() {
 	orm.RegisterDataBase("default", "postgres", beego.AppConfig.String("sqlconn"))
 	if beego.BConfig.RunMode == "dev" {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
+	beego.Run()
+}
+
+func main() {
+	orm.Debug = true
+	orm.RegisterDataBase("default", "postgres", "postgres://"+beego.AppConfig.String("PGuser")+":"+beego.AppConfig.String("PGpass")+"@"+beego.AppConfig.String("PGurls")+"/"+beego.AppConfig.String("PGdb")+"?sslmode=disable&search_path="+beego.AppConfig.String("PGschemas")+"")
+	logPath := "{\"filename\":\""
+	logPath += beego.AppConfig.String("logPath")
+	logPath += "\"}"
+	logs.SetLogger(logs.AdapterFile, logPath)
+	apistatus.Init()
+	beego.ErrorController(&customerror.CustomErrorController{})
+
 	beego.Run()
 }
